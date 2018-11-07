301f92bcc0753973f4ae61a24e50bbcce7e0523e
yongjunpark <windpyj80@gmail.com>
2018-11-07 09:24:45 +0900

    remove file

--- src/main/java/io/github/rxcats/aesrestapidemo/message/EmptyRequest.java
+++
@@ -1,7 +0,0 @@
-package io.github.rxcats.aesrestapidemo.message;
-
-import io.github.rxcats.aesrestapidemo.mvc.converter.AesApiBodyType;
-
-public class EmptyRequest implements AesApiBodyType {
-
-}
